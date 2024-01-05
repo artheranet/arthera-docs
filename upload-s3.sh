b7cfd46e368d434c1b73e9daa98d6026437362c1
flaviusburca <126314488+artheraone@users.noreply.github.com>
2024-01-05 10:19:33 +0200

    New AWS coordinates

--- upload-s3.sh
+++ upload-s3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aws s3 rm s3://docs.arthera.net --recursive
-aws s3 cp build/ s3://docs.arthera.net --recursive
-aws cloudfront create-invalidation --distribution-id E281LOPT7QBU --paths "/*"
+aws s3 rm s3://docs.arthera-net --recursive
+aws s3 cp build/ s3://docs.arthera-net --recursive
+aws cloudfront create-invalidation --distribution-id E1KMW14ZJBNM34 --paths "/*"
 exit
