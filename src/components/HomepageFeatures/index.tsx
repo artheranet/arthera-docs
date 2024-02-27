f277d3fd1772bc78c269de667f428a3cd00e3f3b
Julien Béranger <julien@strat.cc>
2024-02-27 18:23:34 +0100

    Content update (#96)

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -15,8 +15,14 @@ export default function HomepageFeatures(): JSX.Element {
       </div>
 
       <div className={styles.feature}>
-        <h1>Arthera testnet is live</h1>
-        <p>You can check it out using the explorer (<Link href="https://explorer-test2.arthera.net">Blockscout</Link> or <Link href="https://arthera-testnet.socialscan.io">SocialScan</Link>)</p>
+        <h1>Arthera Mainnet</h1>
+        <p>You can check it out using the <Link href="https://explorer.arthera.net">Explorer</Link>.</p>
+        <AddMetamaskNetwork network={'mainnet'} title={'Add Arthera Mainnet to Metamask'} size={'medium'}/>
+      </div>
+
+      <div className={styles.feature}>
+        <h1>Arthera Testnet</h1>
+        <p>You can check it out using the <Link href="https://explorer-test.arthera.net">Explorer</Link>.</p>
         <AddMetamaskNetwork network={'testnet'} title={'Add Arthera Testnet to Metamask'} size={'medium'}/>
       </div>
 
@@ -32,7 +38,7 @@ export default function HomepageFeatures(): JSX.Element {
           <h1>Arthera Explorers</h1>
           <p>Use it to explore the Arthera blockchain: blocks, transactions, tokens, addresses, smart contracts and more.</p>
           <ul>
-          <li>Block explorer (<Link href="https://explorer-test2.arthera.net">Blockscout</Link> or <Link href="https://arthera-testnet.socialscan.io">SocialScan</Link>)</li>
+          <li><Link href="https://explorer-test.arthera.net">Block explorer</Link></li>
             <li><Link href="https://explorer-test-api.arthera.net">Arthera Explorer API</Link></li>
           </ul>
       </div>
