d8d9b899e5594cbc66e8a059bcedd4563d71dd05
Julien Béranger <julien@strat.cc>
2024-02-27 18:20:29 +0100

    Replace `test2` with `test` (#94)

--- src/utils.ts
+++ src/utils.ts
@@ -1,52 +1,61 @@
 import detectEthereumProvider from "@metamask/detect-provider";
 
 const ARTHERA_NETWORK_DETAILS = {
-    'mainnet': {
-        chainId: "0x2802",
-        chainName: "Arthera Mainnet",
-        nativeCurrency: {
-            name: "Arthera",
-            symbol: "AA",
-            decimals: 18,
-        },
-        rpcUrls: ["https://rpc.arthera.net/"],
-        blockExplorerUrls: ["https://explorer.arthera.net/","https://arthera-testnet.socialscan.io/"],
+  mainnet: {
+    chainId: "0x2802",
+    chainName: "Arthera Mainnet",
+    nativeCurrency: {
+      name: "Arthera",
+      symbol: "AA",
+      decimals: 18,
     },
-    'testnet': {
-        chainId: "0x2803",
-        chainName: "Arthera Testnet",
-        nativeCurrency: {
-            name: "Arthera",
-            symbol: "AA",
-            decimals: 18,
-        },
-        rpcUrls: ["https://rpc-test2.arthera.net/"],
-        blockExplorerUrls: ["https://explorer-test2.arthera.net","https://arthera-testnet.socialscan.io/"],
+    rpcUrls: ["https://rpc.arthera.net/"],
+    blockExplorerUrls: [
+      "https://explorer.arthera.net/",
+      "https://arthera-testnet.socialscan.io/",
+    ],
+  },
+  testnet: {
+    chainId: "0x2803",
+    chainName: "Arthera Testnet",
+    nativeCurrency: {
+      name: "Arthera",
+      symbol: "AA",
+      decimals: 18,
     },
-    'devnet': {
-        chainId: "0x2805",
-        chainName: "Arthera Devnet",
-        nativeCurrency: {
-            name: "Arthera",
-            symbol: "AA",
-            decimals: 18,
-        },
-        rpcUrls: ["https://rpc-dev.arthera.net/"],
-        blockExplorerUrls: ["https://explorer-dev.arthera.net","https://arthera-testnet.socialscan.io/"],
-    }
-}
+    rpcUrls: ["https://rpc-test.arthera.net/"],
+    blockExplorerUrls: [
+      "https://explorer-test.arthera.net",
+      "https://arthera-testnet.socialscan.io/",
+    ],
+  },
+  devnet: {
+    chainId: "0x2805",
+    chainName: "Arthera Devnet",
+    nativeCurrency: {
+      name: "Arthera",
+      symbol: "AA",
+      decimals: 18,
+    },
+    rpcUrls: ["https://rpc-dev.arthera.net/"],
+    blockExplorerUrls: [
+      "https://explorer-dev.arthera.net",
+      "https://arthera-testnet.socialscan.io/",
+    ],
+  },
+};
 
 export async function addNetwork(network: string) {
-    const provider = await detectEthereumProvider();
-    if (provider) {
-        try {
-            await (window as any).ethereum.request({
-                method: "wallet_addEthereumChain",
-                params: [ARTHERA_NETWORK_DETAILS[network]],
-            });
-            console.log('added');
-        } catch (addError) {
-            console.error(addError);
-        }
+  const provider = await detectEthereumProvider();
+  if (provider) {
+    try {
+      await (window as any).ethereum.request({
+        method: "wallet_addEthereumChain",
+        params: [ARTHERA_NETWORK_DETAILS[network]],
+      });
+      console.log("added");
+    } catch (addError) {
+      console.error(addError);
     }
+  }
 }
