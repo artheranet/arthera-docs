fb57b425e87b38fe0fc63eb78621fa7c2c36ee1d
Bertrand Juglas <b@juglas.name>
2024-08-30 22:07:39 +0200

    Backup of Epoch 7785

--- upload-s3-genesis.sh
+++ upload-s3-genesis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-aws s3 cp mainnet.g s3://release.arthera-net
+aws s3 cp ../epoch7785.g s3://release.arthera-net
 aws cloudfront create-invalidation --distribution-id EO8HM5GITDD9M --paths "/*"
 exit
