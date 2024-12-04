cfbf0f5e63f88662d282c154b96d8eb4f8d70fda
Bertrand Juglas <b@juglas.name>
2024-12-04 15:54:15 +0100

    improve deployment on AWS S3

--- upload-s3.sh
+++ upload-s3.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
+set -x
 
-aws s3 rm s3://docs.arthera-net --recursive
-aws s3 cp build/ s3://docs.arthera-net --recursive
+aws s3 sync build/ s3://docs.arthera-net --delete --dryrun
+aws s3 sync build/ s3://docs.arthera-net --delete
 aws cloudfront create-invalidation --distribution-id E1KMW14ZJBNM34 --paths "/*"
 exit
